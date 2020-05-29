06dc93a29f2cfebf4dbed1e06c4b7ff9d5a52836
Meleahaiken <meleah.aiken@gmail.com>
2020-05-29 17:19:38 -0400

    Update mad-lib-function.php

--- mad-lib-function.php
+++ mad-lib-function.php
@@ -1,4 +1,7 @@
 <?php
+/* 
+In this project, we’ll use PHP to write a function to fill in a Mad Libs story! Mad Libs are short stories with blank spaces, which get filled in by the user. The result is usually funny (or strange).
+*/
 function generateStory($singular_noun, $verb, $color, $distance_unit)
 {
   $story = "\nThe ${singular_noun}s are lovely, ${color}, and deep.\nBut I have promises to keep,\nAnd ${distance_unit} to go before I ${verb},\nAnd ${distance_unit} to go before I ${verb}.\n";
